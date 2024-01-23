e8d447b03353b02c5203f430d2028cbf2a78fde7
Lianny Coringa <lianny_costa@hotmail.com>
2024-01-23 20:40:48 -0300

    NavBar e Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import NavBar from './NavBar';
+
+function Header() {
+  return (
+    <header>
+      <img src="images/image1.png" alt="lianny coringa" width={ 200 } />
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <a href="/">Início</a>
+      <a href="/about">Sobre mim</a>
+      <a href="/hardskills">Hard skills</a>
+      <a href="/softskills">Soft skills</a>
+      <a href="/projects">Projetos</a>
+      <a href="/contact">Contato</a>
+    </div>
+  );
+}
+
+export default NavBar;
