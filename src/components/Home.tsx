36d7db76212c3d96f759ac5e8acc79a8d46afb3d
Lianny Coringa <lianny_costa@hotmail.com>
2024-01-23 21:31:56 -0300

    Page home iniciando

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 id="title">Olá, me chamo Lianny Coringa</h1>
+      <h2>Bem-vindo ao meu portfólio</h2>
+      <h3>Sou Desenvolvedora Full Stack</h3>
+    </div>
+  );
+}
+
+export default Home;
