5364b9666c1fe427f169783983dc8d69483309ff
Lianny Coringa <lianny_costa@hotmail.com>
2024-01-30 20:12:46 -0300

    Alterando hard e soft skills

--- src/components/HardSkills.tsx
+++ src/components/HardSkills.tsx
@@ -1,33 +1,85 @@
 function HardSkills() {
   return (
-    <div className="hard-skills-container" id="hardskills">
-      <h1>Hard Skills</h1>
-      <div className="hard-skills">
-        <div className="card-container">
-          <h2>Front-end</h2>
-          <div className="card">
+    <div className="hard-skills-container" id="skills">
+      <h1>Skills</h1>
+      <div className="card-container">
+        <h2>Hard Skills</h2>
+        <div className="card">
+          <div className="card-filho">
             <img src="images/git.svg" alt="github" />
+            <p>Git</p>
+          </div>
+          <div className="card-filho">
             <img src="images/html.svg" alt="html" />
+            <p>HTML 5</p>
+          </div>
+          <div className="card-filho">
             <img src="images/css-3.png" alt="css" />
+            <p>CSS 3</p>
+          </div>
+          <div className="card-filho">
             <img src="images/script-java.png" alt="javascript" />
+            <p>JavaScript</p>
+          </div>
+          <div className="card-filho">
             <img src="images/typescript.png" alt="typescript" />
+            <p>TypeScript</p>
+          </div>
+          <div className="card-filho">
             <img src="images/icons8-react-48.png" alt="react" />
+            <p>React</p>
+          </div>
+          <div className="card-filho">
             <img src="images/redux.png" alt="redux" />
+            <p>Redux</p>
+          </div>
+          <div className="card-filho">
             <img src="images/jest.png" alt="jest" />
+            <p>Jest</p>
+          </div>
+          <div className="card-filho">
             <img src="images/vite_logo_icon_249258.png" alt="vite" />
+            <p>Vite</p>
+          </div>
+          <div className="card-filho">
             <img src="images/tailwind_icon_131947.png" alt="tailwind" />
+            <p>Tailwind</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/nodejs.png" alt="node" />
+            <p>Node</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/icons8-docker-50.png" alt="docker" />
+            <p>Docker</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/icons8-mysql-50.png" alt="mysql" />
+            <p>MySQL</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/sequelize_icon_132004.png" alt="sequelize" />
+            <p>Sequelize</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/mocha_plain_logo_icon_146426.png" alt="mocha" />
+            <p>Mocha</p>
+          </div>
+          <div className="card-filho">
+            <img src="images/icons8-python-50.png" alt="python" />
+            <p>Python</p>
           </div>
         </div>
-        <div className="card-container">
-          <h2>Back-end</h2>
-          <div className="card">
-            <img src="images/nodejs.png" alt="node" />
-            <img src="images/icons8-docker-50.png" alt="docker" />
-            <img src="images/icons8-mysql-50.png" alt="mysql" />
-            <img src="images/sequelize_icon_132004.png" alt="sequelize" />
-            <img src="images/mocha_plain_logo_icon_146426.png" alt="mocha" />
-            <img src="images/icons8-python-50.png" alt="python" />
-          </div>
+      </div>
+      <div className="card-container">
+        <h2>Soft Skills</h2>
+        <div className="card-soft">
+          <li>Comunicação</li>
+          <li>Inteligência Emocional</li>
+          <li>Criatividade</li>
+          <li>Colaboração</li>
+          <li>Pensamento crítico</li>
+          <li>Liderança</li>
         </div>
       </div>
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,7 @@ function NavBar() {
     <div className="NavBar">
       <a href="#title"><b>Início</b></a>
       <a href="#aboutus"><b>Sobre mim</b></a>
-      <a href="#hardskills"><b>Hard skills</b></a>
-      <a href="/softskills"><b>Soft skills</b></a>
+      <a href="#skills"><b>Skills</b></a>
       <a href="/projects"><b>Projetos</b></a>
       <a href="/contact"><b>Contato</b></a>
     </div>
