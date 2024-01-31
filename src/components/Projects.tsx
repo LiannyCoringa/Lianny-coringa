4c9dbf9603812b597756d041a86d4163ea79b732
Lianny Coringa <lianny_costa@hotmail.com>
2024-01-30 22:08:25 -0300

    Iniciando o componente Projects

---
+++ src/components/Projects.tsx
@@ -0,0 +1,44 @@
+function Projects() {
+  return (
+    <div>
+      <h1>Projetos</h1>
+      <div>
+        <h2>Front-end</h2>
+        <div>
+          <div>
+            <h3>Password Manager</h3>
+            <p>Tecnologias utilizadas: TypeScript e React</p>
+            <button>Saiba mais</button>
+          </div>
+          <div>
+            <h3>TrybeTunes</h3>
+            <p>Tecnologias utilizadas: TypeScript, React e React Router</p>
+            <button>Saiba mais</button>
+          </div>
+          <div>
+            <h3>Front-end Online Store</h3>
+            <p>Tecnologias utilizadas: TypeScript, React e React Router</p>
+            <button>Saiba mais</button>
+          </div>
+          <div>
+            <h3>TrybeWallet</h3>
+            <p>Tecnologias utilizadas: TypeScript, React e Redux</p>
+            <button>Saiba mais</button>
+          </div>
+          <div>
+            <h3>StarWars</h3>
+            <p>Tecnologias utilizadas: TypeScript, React e Context API</p>
+            <button>Saiba mais</button>
+          </div>
+          <div>
+            <h3>App de Receitas</h3>
+            <p>Tecnologias utilizadas: TypeScript, React e Context API</p>
+            <button>Saiba mais</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
