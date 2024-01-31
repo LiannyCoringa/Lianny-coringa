bf175b3f85a4edb0fa9b52e104d8eefe8d24efc7
Lianny Coringa <lianny_costa@hotmail.com>
2024-01-30 22:59:57 -0300

    primeiro modal

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,32 +8,44 @@ function Projects() {
           <div>
             <h3>Password Manager</h3>
             <p>Tecnologias utilizadas: TypeScript e React</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
+            <div className="modal fade bd-example-modal-lg" tabIndex={-1} role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+              <div className="modal-dialog modal-lg">
+                <div className="modal-content">
+                  <h1>Password Manager</h1>
+                  <p>Este projeto foi desenvolvido com o intuito de gerenciar senhas, podendo adicionar, editar e deletar.</p>
+                  <p>Tecnologias utilizadas: TypeScript e React</p>
+                  <a href="https://github.com/LiannyCoringa/Project-Password-Manager" target="_blank" rel="noreferrer">
+                    <button className="btn btn-primary">Repositório</button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
           <div>
             <h3>TrybeTunes</h3>
             <p>Tecnologias utilizadas: TypeScript, React e React Router</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>Front-end Online Store</h3>
             <p>Tecnologias utilizadas: TypeScript, React e React Router</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>TrybeWallet</h3>
             <p>Tecnologias utilizadas: TypeScript, React e Redux</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>StarWars</h3>
             <p>Tecnologias utilizadas: TypeScript, React e Context API</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>App de Receitas</h3>
             <p>Tecnologias utilizadas: TypeScript, React e Context API</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
         </div>
       </div>
@@ -43,22 +55,22 @@ function Projects() {
           <div>
             <h3>Store Manager</h3>
             <p>Tecnologias utilizadas: JavaScript, Docker, MySQL, Express</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>Blogs API</h3>
             <p>Tecnologias utilizadas: JavaScript, Docker, MySQL, Express</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>Trybe Smith</h3>
             <p>Tecnologias utilizadas: TypeScript, Docker, MySQL, Express</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
           <div>
             <h3>Trybe Futebol Clube</h3>
             <p>Tecnologias utilizadas: TypeScript, React, Docker, MySQL, Express</p>
-            <button>Saiba mais</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Saiba mais</button>
           </div>
         </div>
       </div>
